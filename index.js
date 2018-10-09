82f4fb87f1d2b2cd95e76228b95c720152969b53
Jul Jen <juliajen.prog@gmail.com>
2018-10-09 19:56:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-// new Neighborhood() - initialized with name. It returns an object that has attributes of Id and name
+// new Neighborhood() - initialized with name. It returns an object that has attributes of id and name
 // deliveries() - returns a list of all deliveries placed in a neighborhood
 // customers() - returns all of the customers that live in a particular neighborhood
 // meals() - returns a unique list of meals that have been ordered in a particular neighborhood (you might want to do this one last)
 
-let neighborhoodId = 0
-let mealId = 0
-let customerId = 0
-let deliveryId = 0
+let neighborhoodID = 0
+let mealID = 0
+let customerID = 0
+let deliveryID = 0
 
 
 class Neighborhood {
   constructor(name) {
-    this.Id = ++neighborhoodId;
+    this.id = ++neighborhoodID;
     this.name = name;
     store.neighborhoods.push(this)
   }
 
   customers() {
-    return store.customers.filter(x => {
-      return x.neighborhoodId === this.Id
+    return store.customers.filter(customer => {
+      return customer.neighborhoodId === this.id
     });
   }
 
@@ -32,7 +32,7 @@ class Neighborhood {
     }
 
   deliveries() {
-    return store.deliveries.filter(x => x.neighborhoodId === this.Id)
+    return store.deliveries.filter(x => x.neighborhoodId === this.id)
   }
 }
 
@@ -40,7 +40,7 @@ class Neighborhood {
 //
 // class Neighborhood {
 //   constructor(name) {
-//     this.Id = ++neighborhoodId;
+//     this.id = ++neighborhoodId;
 //     this.name = name;
 //
 //     store.neighborhoods.push(this)
@@ -49,7 +49,7 @@ class Neighborhood {
 //    // A neighborhood has many customers through deliveries
 //   customers() {
 //     return store.customers.filter(customer => {
-//       return customer.neighborhood === this.Id
+//       return customer.neighborhood === this.id
 //     })
 //   }
 //
@@ -62,13 +62,13 @@ class Neighborhood {
 //    // A neighborhood has many deliveries
 //   deliveries(){
 //     return store.deliveries.filter(delivery => {
-//       return delivery.neighborhood === this.Id
+//       return delivery.neighborhood === this.id
 //     })
 //   }
 // }
 //
 //
-// // new Customer() — should expect to be initialized with a name and a neighborhoodId. It returns an object that has attributes of Id, neighborhoodId, and name.
+// // new Customer() — should expect to be initialized with a name and a neighborhoodId. It returns an object that has attributes of id, neighborhoodId, and name.
 // // deliveries() — returns all of the deliveries that customer has received
 // // meals() - returns all meals that a customer has ordered
 // // totalSpent() - returns the total amount that the customer has spent on food.
@@ -76,23 +76,23 @@ class Neighborhood {
 //
 // class Customer {
 //   constructor(name, neighborhoodId) {
-//     this.Id = ++customerId;
+//     this.id = ++customerId;
 //     this.name = name;
 //     if(neighborhood) {
-//       this.neighborhoodId = neighborhood.Id;
+//       this.neighborhoodId = neighborhood.id;
 //     }
 //
 //     store.customers.push(this)
 //   }
 //   deliveries(){
 //     return store.customers.filter(delivery => {
-//       return delivery.cutomerId === this.Id
+//       return delivery.cutomerId === this.id
 //     })
 //   }
 // }
 //
 //
-// // new Meal() — initialized with title and price. It returns an object that has attributes of title, price, and Id. Meal Ids should automatically increment.
+// // new Meal() — initialized with title and price. It returns an object that has attributes of title, price, and id. Meal Ids should automatically increment.
 // // deliveries() - returns all of the deliveries associated with a particular meal.
 // // customers() - returns all of the customers who have had the meal delivered. Be careful not to return the same customer twice if they have ordered this meal multiple times.
 // // byPrice() - A class method that orders all meal instances by their price in descending order. Use the static keyword to write a class method.
@@ -101,7 +101,7 @@ class Neighborhood {
 //
 // class Meal {
 //   constructor(title, price) {
-//     this.Id = ++mealId;
+//     this.id = ++mealId;
 //     this.title = title;
 //     this.price = price;
 //
@@ -110,7 +110,7 @@ class Neighborhood {
 // }
 //
 //
-// // new Delivery() — initialized with mealId, neighborhoodId, and customerId. It returns an object that has attributes of mealId, neighborhoodId, customerId, and Id
+// // new Delivery() — initialized with mealId, neighborhoodId, and customerId. It returns an object that has attributes of mealId, neighborhoodId, customerId, and id
 // // meal() - returns the meal associated with a particular delivery
 // // customer() - returns the customer associated with a particular delivery
 // // neighborhood() - returns the neighborhood associated with a particular delivery
@@ -119,15 +119,15 @@ class Neighborhood {
 //
 // class Delivery {
 //   constructor(mealId, neighborhoodId, customerId) {
-//     this.Id = ++deliverylId;
+//     this.id = ++deliverylId;
 //     if(meal) {
-//       this.mealId = meal.Id;
+//       this.mealId = meal.id;
 //     }
 //     if(neighborhood) {
-//       this.neighborhoodId = neighborhood.Id;
+//       this.neighborhoodId = neighborhood.id;
 //     }
 //     if(customer) {
-//       this.customer = customer.Id;
+//       this.customer = customer.id;
 //     }
 //     store.deliveries.push(this)
 //   }
@@ -141,18 +141,18 @@ class Neighborhood {
 
 class Meal {
   constructor(title, price) {
-    this.Id = ++mealId
+    this.id = ++mealID
     this.title = title
     this.price = price
     store.meals.push(this)
   }
 
   deliveries() {
-    return store.deliveries.filter(x => x.mealId === this.Id)
+    return store.deliveries.filter(x => x.mealId === this.id)
   }
 
   // customers() {
-  //   return store.customers.filter(x => x.mealId === this.Id)
+  //   return store.customers.filter(x => x.mealId === this.id)
   // }
 
   customers() {
@@ -168,15 +168,15 @@ class Meal {
 
 
 class Customer {
-  constructor(name, neighborhoodId){
-    this.Id = ++customerId
+  constructor(name, neighborhoodID){
+    this.id = ++customerID
     this.name = name
-    this.neighborhoodId = neighborhoodId
+    this.neighborhoodId = neighborhoodID
     store.customers.push(this)
   }
 
   deliveries() {
-    return store.deliveries.filter(x => x.customerId === this.Id)
+    return store.deliveries.filter(x => x.customerId === this.id)
   }
 
   meals() {
@@ -190,24 +190,24 @@ class Customer {
 
 
 class Delivery {
-  constructor(mealId, neighborhoodId, customerId,){
-    this.Id = ++deliveryId
-    this.mealId = mealId
-    this.neighborhoodId = neighborhoodId
-    this.customerId = customerId
+  constructor(mealID, neighborhoodID, customerID,){
+    this.id = ++deliveryID
+    this.mealId = mealID
+    this.neighborhoodId = neighborhoodID
+    this.customerId = customerID
     store.deliveries.push(this)
   }
 
   meal() {
-    return store.meals.find(meal => meal.Id === this.mealId)
+    return store.meals.find(meal => meal.id === this.mealId)
   }
 
   neighborhood() {
-    return store.neighborhoods.find(neighborhood => neighborhood.Id === this.neighborhoodId)
+    return store.neighborhoods.find(neighborhood => neighborhood.id === this.neighborhoodId)
   }
 
   customer() {
-    return store.customers.find(customer => customer.Id === this.customerId)
+    return store.customers.find(customer => customer.id === this.customerId)
   }
 
 }
