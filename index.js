8ce9d55b9347c80d2e29e36266e758d25488704a
Jul Jen <juliajen.prog@gmail.com>
2018-10-09 19:47:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ class Neighborhood {
   }
 
   customers() {
-    return store.customers.filter(customer => {return customer.neighborhoodId === this.id})
+    return store.customers.filter(customer => {
+      return customer.neighborhoodId === this.id
+    });
   }
 
 
