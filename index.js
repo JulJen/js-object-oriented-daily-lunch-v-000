b58bf63ca187e1e44aea419cd7d1afbaef3ba8af
Jul Jen <juliajen.prog@gmail.com>
2018-10-09 19:45:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// // global datastore
-// let store = { neighborhoods: [], customers: [],  meals: [], deliveries: [] };
-// 
+// global datastore
+let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+//
 // // new Neighborhood() - initialized with name. It returns an object that has attributes of id and name
 // // deliveries() - returns a list of all deliveries placed in a neighborhood
 // // customers() - returns all of the customers that live in a particular neighborhood
 // // meals() - returns a unique list of meals that have been ordered in a particular neighborhood (you might want to do this one last)
-// 
+//
 // let neighborhoodId = 0
 // let mealId = 0
 // let customerId = 0
 // let deliverylId = 0
-// 
+//
 // class Neighborhood {
 //   constructor(name) {
 //     this.id = ++neighborhoodId;
 //     this.name = name;
-// 
+//
 //     store.neighborhoods.push(this)
 //   }
 //    // Customer belongs to neighborhood
@@ -25,13 +25,13 @@
 //       return customer.neighborhood === this.id
 //     })
 //   }
-// 
+//
 //   meals() {
 //     const allMeals = this.customers().map(customer => customer.meals());
 //     const merged = [].concat.apply([], allMeals);
 //     return [...new Set(merged)];
 //   }
-// 
+//
 //    // A neighborhood has many deliveries
 //   deliveries(){
 //     return store.deliveries.filter(delivery => {
@@ -39,14 +39,14 @@
 //     })
 //   }
 // }
-// 
-// 
+//
+//
 // // new Customer() — should expect to be initialized with a name and a neighborhoodId. It returns an object that has attributes of id, neighborhoodId, and name.
 // // deliveries() — returns all of the deliveries that customer has received
 // // meals() - returns all meals that a customer has ordered
 // // totalSpent() - returns the total amount that the customer has spent on food.
-// 
-// 
+//
+//
 // class Customer {
 //   constructor(name, neighborhoodId) {
 //     this.id = ++customerId;
@@ -54,7 +54,7 @@
 //     if(neighborhood) {
 //       this.neighborhoodId = neighborhood.id;
 //     }
-// 
+//
 //     store.customers.push(this)
 //   }
 //   deliveries(){
@@ -63,33 +63,33 @@
 //     })
 //   }
 // }
-// 
-// 
+//
+//
 // // new Meal() — initialized with title and price. It returns an object that has attributes of title, price, and id. Meal Ids should automatically increment.
 // // deliveries() - returns all of the deliveries associated with a particular meal.
 // // customers() - returns all of the customers who have had the meal delivered. Be careful not to return the same customer twice if they have ordered this meal multiple times.
 // // byPrice() - A class method that orders all meal instances by their price in descending order. Use the static keyword to write a class method.
-// 
-// 
-// 
+//
+//
+//
 // class Meal {
 //   constructor(title, price) {
 //     this.id = ++mealId;
 //     this.title = title;
 //     this.price = price;
-// 
+//
 //     store.meals.push(this)
 //   }
 // }
-// 
-// 
+//
+//
 // // new Delivery() — initialized with mealId, neighborhoodId, and customerId. It returns an object that has attributes of mealId, neighborhoodId, customerId, and id
 // // meal() - returns the meal associated with a particular delivery
 // // customer() - returns the customer associated with a particular delivery
 // // neighborhood() - returns the neighborhood associated with a particular delivery
-// 
-// 
-// 
+//
+//
+//
 // class Delivery {
 //   constructor(mealId, neighborhoodId, customerId) {
 //     this.id = ++deliverylId;
@@ -109,8 +109,7 @@
 
 
 
-// global datastore
-let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
 
 let neighborhoodID = 0
 let mealID = 0
