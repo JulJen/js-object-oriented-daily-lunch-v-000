2845f2a0ab95c244bf7a4d1c10a4f3e2f7838a96
Jul Jen <juliajen.prog@gmail.com>
2018-10-09 20:04:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,6 +40,23 @@ class Neighborhood {
 }
 
 
+class Customer {
+  constructor(name, neighborhoodId) {
+    this.id = ++customerId;
+    this.name = name;
+    if(neighborhood) {
+      this.neighborhoodId = neighborhood.id;
+    }
+
+    store.customers.push(this)
+  }
+  deliveries(){
+    return store.customers.filter(delivery => {
+      return delivery.cutomerId === this.id
+    })
+  }
+}
+
 
 
 class Customer {
@@ -64,23 +81,6 @@ class Customer {
 }
 
 
-
-class Customer {
-  constructor(name, neighborhoodId) {
-    this.id = ++customerId;
-    this.name = name;
-    if(neighborhood) {
-      this.neighborhoodId = neighborhood.id;
-    }
-
-    store.customers.push(this)
-  }
-  deliveries(){
-    return store.customers.filter(delivery => {
-      return delivery.cutomerId === this.id
-    })
-  }
-}
 
 
 // // new Meal() — initialized with title and price. It returns an object that has attributes of title, price, and id. Meal Ids should automatically increment.
